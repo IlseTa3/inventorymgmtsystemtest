845850404dd146a67a3069f56bb749e57858726e
Ilse Tastenhoye <ilse.tastenhoye@gmail.com>
2024-09-30 23:03:13 +0200

    Added Identity with Register and Login

--- TestInventoryMgmtSystem/Program.cs
+++ TestInventoryMgmtSystem/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using TestInventoryMgmtSystem.Models; // Zorg ervoor dat je de juiste namespace gebruikt
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,9 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
     new MySqlServerVersion(new Version(8, 0, 36))));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
@@ -22,8 +26,12 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
 app.UseRouting();
+
 app.UseAuthorization();
+app.UseAuthentication();
+app.MapRazorPages();
 
 app.MapControllerRoute(
     name: "default",
